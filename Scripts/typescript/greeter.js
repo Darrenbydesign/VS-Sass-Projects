e8734a9ba0abbb487cc368502060e6ba1e1f460a
darrenbydesign <darrenbydesign@gmail.com>
2015-12-09 14:38:46 -0600

    switched from a css folder to a styles folder for compiled CSS adds typescript

---
+++ Scripts/typescript/greeter.js
@@ -0,0 +1,8 @@
+﻿function greeter(person) {
+    return "Hello, " + person;
+}
+
+var user = "Super User";
+
+document.body.innerHTML = greeter(user);
+//# sourceMappingURL=greeter.js.map
--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,36 @@
 /// <vs SolutionOpened='default' />
 //Requirements for the project gulp tasks to work
 var gulp = require('gulp');
+
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
-//var autoprefixer = require('gulp-autoprefixer');
 var sassdoc = require('sassdoc');
-//var jade = require('gulp-jade');
+
+var ts = require('gulp-typescript');
+
+gulp.task('scripts', function () {
+    return gulp
+    .src(tsInput)
+    .pipe(ts(typescriptOptions))
+    .pipe(gulp.dest('built/local'));
+});
 
 
 
 //Variables 
 var sassInput = 'sass/**/*.scss'; //This finds the scss files in the sass folder for compilation
 
-var sassOutput = './css'; //This outputs the compiled css files into the CSS folder
+var sassOutput = './styles'; //This outputs the compiled css files into the CSS folder
 
-//var jadeInput = './jade/**/*.jade';
-//var jadeOutput = './Views/Static';
-//var jadeOptions = {
-//   jade: jade,
-//    pretty: true
-//}
+var tsInput = 'typescript/**/*.ts'; //This finds the typescript files in the typescript folder for compilation
+
+// JSON options for compiling Sass files
 var sassOptions = {
     errLogToConsole: true,
     outputStyle: 'expanded'
 }; // Options that are passed to the compilation 
 
-var autoprefixerOptions = {
-    browsers: ['last 2 versions', '> 5%', 'FIrefox ESR']
-}; // autoprefixer options variable when using the autoprefixer option in the css files
-
+// JSON options for compiling Sassdoc files
 var sassdocOptions = {
     dest: './sassdoc',
     verbose: true,
@@ -36,19 +38,11 @@ var sassdocOptions = {
 
 }; // adds values to the sassdoc to set the path for the documentation
 
-
-// Gulp Task to compile sass files using gulp-sass provides extra options and logs errors when compile fails
-gulp.task('sass', function () {
-    return gulp
-        .src(sassInput)
-        .pipe(sourcemaps.init())
-        .pipe(sass(sassOptions)
-        .on('error', sass.logError))
-        .pipe(sourcemaps.write('./maps'))
-        //.pipe(autoprefixer(autoprefixerOptions))
-        .pipe(gulp.dest(sassOutput));
-});
-
+// JSON options for compiling Typescript files
+var typescriptOptions = {
+    noImplicityAny: true,
+    out: 'output.js'
+};
 
 // Seperate task to create sassdoc  documentation
 gulp.task('sassdoc', function () {
@@ -58,38 +52,39 @@ gulp.task('sassdoc', function () {
         .resume();
 });
 
-
 // Gulp Task that watches the Sass files and compiles CSS after a file is saved
-gulp.task('watch', function () {
+gulp.task('sass-watch', function () {
     return gulp
-    // Step 1. Watch Folder
-    // Step 2. Run 'sass' task whenver a change happens
-    // Step 3. Log a message in the console when a change is made
-    .watch(sassInput, ['sass'])
-    //.watch(jadeInput, ['jade'])
-    .on('change', function (event) {
-        console.log('File' + event.path + 'was' + event.type + ', running tasks...');
-    });
+        // Step 1. Watch Folder
+        // Step 2. Run 'sass' task whenver a change happens
+        // Step 3. Log a message in the console when a change is made
+        .watch(sassInput, ['sass'])
+        //.watch(jadeInput, ['jade'])
+        .on('change', function (event) {
+            console.log('File' + event.path + 'was' + event.type + ', running tasks...');
+        });
 });
 
-// Build settings for production
+// Gulp Task to compile sass files using gulp-sass provides extra options and logs errors when compile fails
+gulp.task('sass', function () {
+    return gulp
+        .src(sassInput)
+        .pipe(sourcemaps.init())
+        .pipe(sass(sassOptions)
+        .on('error', sass.logError))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest(sassOutput));
+});
+
+// Build settings for compiling new Sass Documentation in production
 gulp.task('prod', ['sassdoc'], function () {
     return gulp
-       .src(sassInput)
+        .src(sassInput)
         .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(gulp.dest(sassOutput));
 });
 
 
-// Gulp task that runs gulp jade in the CLI
-//gulp.task('jade', function () {
-//    return gulp
-//        .src(jadeInput)
-//        .pipe(jade(jadeOptions))
-//        .pipe(gulp.dest(jadeOutput))
-//        .on('error', gutil.log);
-//});
-
 // Default task that runs watch files that can be compiled
 // more watch tasks can be added to this default task ... e.g a 'jade' watch can added to utilize that function
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['prod', 'sass-watch']);
